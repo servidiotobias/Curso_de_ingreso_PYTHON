58a9dbda417e4b3c238a8dc13e04368cb31fd3a7
servidiotobias <servidiotobias@gmail.com>
2023-07-20 02:42:34 -0300

    ejercicio hecho

--- ejercicios/04_intruccion_while/ejercicio_10/While_app_10.py
+++ ejercicios/04_intruccion_while/ejercicio_10/While_app_10.py
@@ -33,9 +33,39 @@ class App(customtkinter.CTk):
 
 
     def btn_comenzar_ingreso_on_click(self):
-        pass
+        numero_ingresado = ""
+        suma_negativos = 0
+        suma_positivos = 0
+        numeros_positivos = 0
+        numeros_negativos = 0
+        cantidad_0 = 0
+        diferencia_positivos_negativos = 0
 
-    
+        while numero_ingresado != None:
+            numero_ingresado = prompt(title="ej10", prompt="ingrese un numero")
+
+            if numero_ingresado != None:
+                numero_ingresado = int(numero_ingresado)
+
+                if numero_ingresado < 0:
+                    suma_negativos = suma_negativos + numero_ingresado    
+                    numeros_negativos = numeros_negativos + 1
+                elif numero_ingresado > 0:
+                    suma_positivos = suma_positivos + numero_ingresado
+                    numeros_positivos = numeros_positivos + 1
+                else: 
+                    cantidad_0 += 1
+        diferencia_positivos_negativos = numeros_positivos - numeros_negativos
+
+        mensaje = "la suma acumulada de los negativos es {0}".format(suma_negativos)
+        mensaje = mensaje + "la suma acumulada de los positivos es {0}".format(suma_positivos)                
+        mensaje = mensaje + " cantidad de numeros positivos ingresados{0}".format(numeros_positivos)
+        mensaje = mensaje + " cantidad de numeros negativos ingresados{0}".format(numeros_negativos)
+        mensaje = mensaje + " cantidad de ceros{0}".format(cantidad_0)
+        mensaje = mensaje + " diferencia entre la cantidad de los numeros positivos ingresados y negativos{0}".format(diferencia_positivos_negativos)
+
+        alert("datos", mensaje)
+
 if __name__ == "__main__":
     app = App()
     app.geometry("300x300")
